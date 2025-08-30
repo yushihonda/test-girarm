cd4645af86d4302d40fdba5f1637b3ef5677439a
yushihonda <kitasan20201023@gmail.com>
2025-08-30 19:35:08 +0900

    Initial Commit

---
+++ Girarm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Girarm
+//
+//  Created by 本多優士 on 2025/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GirarmTests/GirarmTests.swift
@@ -0,0 +1,17 @@
+//
+//  GirarmTests.swift
+//  GirarmTests
+//
+//  Created by 本多優士 on 2025/08/30.
+//
+
+import Testing
+@testable import Girarm
+
+struct GirarmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
